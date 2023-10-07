963254d9512030e6020081ed07afbc928805e6aa
21.11.4346_Ahmad <ahmadfauzan.280.af@gmail.com>
2023-10-08 02:05:17 +0700

    finalchapter4

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -51,11 +51,11 @@ export const Detail = () => {
           {/* Isi detail movie */}
           <div className="absolute bottom-0 w-1/2 h-2/3 ml-7">
             <div className="text-white absolute top-0 font-sans flex flex-col gap-5 items-baseline h-full">
-              <h1 className="text-5xl font-semibold mb-2">{detailMovie.title}</h1>
+              <h1 className="text-8xl font-semibold mb-2">{detailMovie.title}</h1>
               <p className="text-2xl text-white mb-4">{detailMovie.genres.map((genre) => genre.name).join(", ")}</p>
-              <p className="text-white mt-4 text-lg w-[]">{detailMovie.overview}</p>
-              <p className="text-white mt-4 text-lg">Rating: {detailMovie.vote_average}</p>
-              <p className="text-white mt-4 text-lg">Release Date: {detailMovie.release_date}</p>
+              <p className="text-white mt-4 text-2xl w-[]">{detailMovie.overview}</p>
+              <p className="text-white mt-4 text-2xl">Rating: {detailMovie.vote_average} / 10</p>
+              <p className="text-white mt-4 text-2xl">Release Date: {detailMovie.release_date}</p>
               {trailer && (
                 <a href={trailer} target="_blank" rel="noopener noreferrer" className="w-[20%] flex justify-center bg-red-600 text-white rounded-full px-4 py-2 mt-4 font-semibold hover:bg-red-700">
                   Watch Trailer
